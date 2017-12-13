8eb1146be452df491ff0abaf398838aef2509070
Louisa Xue <xuelex@gmail.com>
2017-12-13 08:08:02 -0800

    Update dessert spec

--- W2D3/spec/dessert_spec.rb
+++ W2D3/spec/dessert_spec.rb
@@ -6,37 +6,59 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 =end
 
 describe Dessert do
-  let(:chef) { double("chef") }
+  let(:chef) { double("chef", name: "victor zhao") }
+  let(:red_velvet) { Dessert.new("cake", 25, chef)}
 
   describe "#initialize" do
-    it "sets a type"
+    it "sets a type" do
+      expect (red_velvet.type).to be("cake")
+    end
 
-    it "sets a quantity"
+    it "sets a quantity" do
+      expect (red_velvet.quantity).to be_a(25)
+    end
 
-    it "starts ingredients as an empty array"
+    it "starts ingredients as an empty array" do
+      expect (red_velvet.ingredients).to be([])
+    end
 
-    it "raises an argument error when given a non-integer quantity"
+    it "raises an argument error when given a non-integer quantity" do
+      expect { Dessert.new("cookie", "chocolate", chef)  }.to raise_error(ArgumentError)
+    end
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    it "adds an ingredient to the ingredients array" do
+      brownie.add_ingredient("flour")
+      expect red_velvet.ingredient("flour").to include("flour")
+    end
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    it "shuffles the ingredient array" do
+      red_velvet.mix
+      expect red_velvet.
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
+    it "subtracts an amount from the quantity" do
+      red_velvet.eat(10)
+      expect red_velvet.amount.to eq(15)
 
-    it "raises an error if the amount is greater than the quantity"
+    it "raises an error if the amount is greater than the quantity" do
+      expect { red_velvet.eat(20) }.to raise_error(ArgumentError)
+    end
   end
 
   describe "#serve" do
-    it "contains the titleized version of the chef's name"
+    it "contains the titleized version of the chef's name" do
+      red_velvet.serve
+      expect "Victor Zhao the Great Baker has made 25 cakes!"
   end
 
   describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      except red_velvet.to receive(:make_more)
+    end
   end
 end
