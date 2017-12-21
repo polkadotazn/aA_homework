c26f866841eb191233b5707ff94a0b1c28cfe2aa
Louisa Xue <xuelex@gmail.com>
2017-12-20 22:33:45 -0800

    Initial commit

--- W3D4/active_record/movie_novice/queries.rb
+++ W3D4/active_record/movie_novice/queries.rb
@@ -23,6 +23,16 @@
 
 def find_angelina
   #find Angelina Jolie by name in the actors table
+  angie = Actor.find_by_sql(<<-SQL)
+    SELECT
+      *
+    FROM
+      actors
+    WHERE
+      name = 'Angelina Jolie'
+  SQL
+
+  angie.first
 
 end
 
@@ -30,12 +40,26 @@ def top_titles
   # get movie titles from movies with scores greater than or equal to 9
   # hint: use 'select' and 'where'
 
+  Movie.select("id, title").where("score >= 9")
+
+
+  # top = Movie.find_by_sql(<<-SQL)
+  #   SELECT
+  #     id, title
+  #   FROM
+  #     movies
+  #   WHERE
+  #     score >= 9
+  # SQL
+  #
+  # top
+
 end
 
 def star_wars
   #display the id, title and year of each Star Wars movie in movies.
   # hint: use 'select' and 'where'
-
+  Movie.select("id, title, yr").where("title LIKE ?", "%Star Wars%")
 end
 
 
@@ -44,6 +68,21 @@ def below_average_years
   #with the count of movies scoring under 5 aliased as bad_movies,
   #in descending order
   # hint: use 'select', 'where', 'group', 'order'
+  bay = Movie.find_by_sql(<<-SQL)
+    SELECT
+      yr, count(*) as bad_movies
+    FROM
+      movies
+    WHERE
+      score < 5
+    GROUP BY
+      yr
+    ORDER BY
+      bad_movies DESC
+
+  SQL
+
+  bay
 
 end
 
@@ -53,14 +92,14 @@ def alphabetized_actors
   # Note: Ubuntu users may find that special characters
   # are alphabetized differently than the specs.
   # This spec might fail for Ubuntu users. It's ok!
-
+  Actor.select("actors.id, name").order("name").limit(10)
 end
 
 def pulp_fiction_actors
   # practice using joins
   # display the id and name of all actors in the movie Pulp Fiction
   # hint: use 'select', 'joins', 'where'
-
+  Actor.select("actors.id, name").joins(:movies).where("title='Pulp Fiction'")
 end
 
 def uma_movies
@@ -68,5 +107,6 @@ def uma_movies
   # display the id, title, and year of movies Uma Thurman has acted in
   # order them by ascending year
   # hint: use 'select', 'joins', 'where', and 'order'
-
+  Movie.select("movies.id, title, yr").joins(:actors).where("name='Uma Thurman'")
+  .order("yr ASC")
 end
