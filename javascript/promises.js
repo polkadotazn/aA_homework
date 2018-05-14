6373d1bb9d16a9f6633acfae83552ff462e81cdc
Louisa Xue <xuelex@gmail.com>
2018-05-14 08:19:31 -0400

    Promise work

--- javascript/promises.js
+++ javascript/promises.js
@@ -8,12 +8,39 @@ require('es6-promise');
 //   console.log('TIMED OUT!');
 // }, 300);
 
-// (2) Fulfilling a Promise
+// (2) Fulfilling a Promise / (3) Rejecting a Promise
+
+// var promise = new Promise (function(fulfill, reject) {
+//   setTimeout( () => {
+//     reject(new Error('REJECTED!'));
+//   }, 300);
+// });
+//
+// function onReject(error) {
+//   console.log(error.message);
+// }
+//
+// promise.then(console.log, onReject);
+
+// (4) To reject or not to reject
+
+// var promise = new Promise (function(fulfill, reject) {
+//   fulfill('I FIRED');
+//   reject(new Error('I DID NOT FIRE'));
+// });
+//
+// function onRejected(error) {
+//   console.log(error.message);
+// }
+//
+// promise.then(console.log, onRejected);
+
+// (5) Always asynchronous
 
 var promise = new Promise (function(fulfill, reject) {
-  setTimeout( () => {
-    fulfill('FULFILLED');
-  }, 300);
+  fulfill('PROMISE VALUE');
 });
 
+
 promise.then(console.log);
+console.log("MAIN PROGRAM");
